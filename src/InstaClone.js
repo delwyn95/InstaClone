4feab33366e98c07f52eba780cd78d058f08e57a
Delwyn <khmer.delwyn.9523@gmail.com>
2019-03-06 16:14:28 -0500

    Added iconBar + index.js

--- src/InstaClone.js
+++ src/InstaClone.js
@@ -1,35 +1,55 @@
 import React, {Component} from 'react';
-import { View, Text, StyleSheet, Image } from 'react-native';
+import { View, Text, StyleSheet, Image, Dimensions } from 'react-native';
+import config from './config';
 
 class InstaClone extends Component {
+    constructor(){
+        super();
+            this.state = {
+                screenWidth: Dimensions.get("window").width
+            };
+    }
+
 
     render(){ 
+        const imageHeight = Math.floor(this.state.screenWidth * 1.1);
+        const imageUri = "https://lh3.googleusercontent.com/AMCbjqDdNvCR6Y_kyio_uwejcX2AFevF8NMucY5YDrUSL6BVNoeDhbYX8ZEhp0Yvm8jCszGsif-HlwfoH9oOpMLO4A" + 
+        "=s" + 
+        imageHeight +
+        "-c";
+        
         return( 
+    
             <View style = {{flex: 1, width:100 + "%", height: 100 + "%" }}>
             <View style={styles.tempNav}>
-                <Text > Uglygram</Text>    
+                <Text>Uglygram</Text>    
             </View> 
             <View style={styles.userBar}> 
             
             <View style = {{ flexDirection: "row", alignItems: 'center', }}>
                 <Image 
-                style = {styles.userPic}
+                style={styles.userPic}
                 source = {{
                     uri:"https://lh3.googleusercontent.com/mg_fwh_obOFCS_E42lRSb7V-JUzisbmnav4v87whIyywy5ymPS-mQRYIjhCq2qs5F4W6TxnV4SCP7HRErcfGjQKTXJw"
                     }}
                 />
                 <Text style= {{marginLeft: 5}}>Gayake.Yit</Text>
+                
             </View>
-         
+            <View style={{alignItems: "center"}}>
+                <Text style={{ fontSize: 30 }}>
+                    ...
+                </Text>
             </View>
-            
+
+            </View>
             <Image 
-                style={{ width: 100 + "%", height: 100}}
+                style={{ width: this.state.screenWidth, height: 400 }}
                 source={{
-                    uri:
-                        "https://lh3.googleusercontent.com/AMCbjqDdNvCR6Y_kyio_uwejcX2AFevF8NMucY5YDrUSL6BVNoeDhbYX8ZEhp0Yvm8jCszGsif-HlwfoH9oOpMLO4A"
+                    uri: imageUri
                     }}
             />
+            <View></View>
             </View>
 
         )
@@ -45,14 +65,16 @@ const styles = StyleSheet.create({
         borderBottomColor: "rgb(233,233,233)",
         borderBottomWidth: StyleSheet.hairlineWidth,
         justifyContent:"center",
-        alignItems: "center" 
+        alignItems: "center" ,
+     
     },
     userBar:{
         width: 100 + '%',
-        height: 50,
+        height: config.styleConstants.rowHeight,
         backgroundColor: "rgb(255,255,255)",
         flexDirection: 'row',
-        marginHorizontal: 10,
+        paddingHorizontal: 10,
+        justifyContent:"space-between"
     },
     userPic:{
         height: 40,
---
+++ src/config/index.js
@@ -0,0 +1,12 @@
+export default{
+    images:{
+        starIcon: require("../../assets/star.png"),
+        starredIcon: require("../../assets/starred.png"),
+        chatIcon: require("../../assets/bubble.png"),
+        arrowIcon: require("../../assets/arrow.png")
+    },
+    styleConstants: {
+        rowHeight: 50
+    }
+
+};
